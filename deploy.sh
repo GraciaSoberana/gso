7e483d5b1f02203a0e9ebbc5372871a7c130f26a
GraciaSoberana <graciasoberanaorizaba@gmail.com>
2026-05-16 14:36:25 -0600

    chore: hacer deploy.sh más robusto con set -e y manejo de permisos

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ mv content_tmp content
 echo "Subiendo a GitHub..."
 git add content/
 git commit -m "actualización $(date '+%Y-%m-%d %H:%M')" || echo "Nada que commitear."
-git push --force https://$GITHUB_TOKEN@github.com/GraciaSoberana/gso.git main
+git push https://$GITHUB_TOKEN@github.com/GraciaSoberana/gso.git main
 
 echo "¡Listo! El sitio se actualizará en unos minutos."
